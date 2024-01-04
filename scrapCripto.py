fb1789a2e65b187f466cf97a793e86da05f44341
otluiz <olto@ecomp.poli.br>
2024-01-04 14:53:28 -0300

    primeiro scrap

---
+++ scrapCripto.py
@@ -0,0 +1,27 @@
+#
+# Esta é uma simples implementação de um web scraping para constuir
+# um grá fico das principais cripto moedas do site coingecko:
+# url = "https://www.coingecko.com/en/crypto-gainers-losers"
+#
+
+import requests
+from bs4 import BeautifulSoup
+
+url = "https://www.coingecko.com/en/crypto-gainers-losers"
+response = requests.get(url)
+html_content = response.text
+
+soup = BeautifulSoup(html_content, 'html.parser')
+
+for coin in soup.find_all("tr"):
+    # Tente extrair as informações necessárias de cada <tr>
+    # Por exemplo, para o nome da moeda
+    name = coin.find("span", class_="lg:tw-flex font-bold tw-items-center tw-justify-between")
+    if name:
+        name = name.text
+        print(name)
+    # Adicione código similar para extrair outras informações, como preço, variação, etc.
+
+
+
+
